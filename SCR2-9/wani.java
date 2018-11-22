2866d4fff02d75ff2a479f3fe0c077c404d76ee3
安田 陵人 <14178@jnet40-n.jnet.hakodate-ct.ac.jp>
2018-11-22 11:42:39 +0900

    下移動

--- SCR2-9/wani.java
+++ SCR2-9/wani.java
@@ -14,6 +14,9 @@ public class wani extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(1);
+}// Add your action code here.
     }    
 }
